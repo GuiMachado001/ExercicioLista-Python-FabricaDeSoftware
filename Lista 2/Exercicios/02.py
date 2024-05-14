03eda5fa512e9bec523aed96e5df94fb6146fd4d
Guilherme Machado Neves <144240599+Guilherme-Machado01@users.noreply.github.com>
2024-05-14 11:42:54 -0400

    Respostas Lista2 - Algoritimo Fabrica de Sorftware

---
+++ Lista 2/Exercicios/02.py
@@ -0,0 +1,10 @@
+#Crie um programa que receba um número inteiro e verifique se ele é maior que 10 se sim, 
+#imprima: é maior que 10, senão imprima: é menor que 10
+
+num = int(input("Digite um numero inteiro: "))
+
+if(num > 10):
+    print("É maior que 10")
+
+else:
+    print("Seu numero é menor ou igual a 10")
---
+++ Lista 2/Exercicios/08.py
@@ -0,0 +1,6 @@
+# Leia um número fornecido pelo usuário. Se esse número for positivo, calcule a raiz quadrada do 
+# número. Se o número for negativo, mostre uma mensagem dizendo que o número é inválido.
+
+
+num = float(input("Digite o um numero: "))
+

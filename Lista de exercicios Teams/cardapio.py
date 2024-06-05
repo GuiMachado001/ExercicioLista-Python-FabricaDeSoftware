f043e6f3c26fb094ad64b42285d7fa98a27af940
Guilherme Machado <gui.m.neves01@gmail.com>
2024-06-04 23:38:25 -0400

    Resolução Cardápio

--- Lista de exercicios Teams/cardapio.py
+++ Lista de exercicios Teams/cardapio.py
@@ -5,7 +5,6 @@
 # “Aceita pagar a gorjeta do garçom 10% sobre o valor do prato”. Se o usuário aceitar, mostrar
 # o valor final (valor do prato + 10%), caso contrário, mostrar o valor final 
 # (somente o valor do prato). 
-import match  
 
 cardapioLista = [
     {'Bife Acebolado': 15.00},
@@ -16,15 +15,115 @@ cardapioLista = [
     {'Lasanha': 22.90}
 ]
 
-
-cod = 200
+codigo = 200 
 
 for item in cardapioLista:
     nome = list(item.keys())[0]
     valor = item[nome]
-    print(f"codigo: {cod} Nome: {nome}, Valor: R$ {valor:.2f}")
-    cod += 1
+    print(f"codigo: {codigo} Nome: {nome}, Valor: R$ {valor:.2f}")
+    codigo += 1
 
-match cod:
-    case 200:
-        print('Bi')
+
+sep = ("_____________________________________")
+
+finalizar = True
+fecharPedido = True
+pedidoAtual = []
+
+
+while(finalizar):
+    
+    cod = int(input("Digite o codigo para adicionar o produto ou '0' para finalizar o pedido: "))
+
+    if(cod == 0):
+        finalizar = False
+
+    else:
+        print(sep)
+        match cod:
+            case 200:
+                print(f'{list(cardapioLista[0].keys())[0]} R$ {list(cardapioLista[0].values())[0]}')
+                quant = int(input("Digite a Quantidade do produto: "))
+                pedidoAtual.append(quant*list(cardapioLista[0].values())[0])
+
+                TotalSemTaxa = sum(pedidoAtual)
+                print(f"Valor Total: R${TotalSemTaxa}")
+                
+                print(sep)
+
+            case 201: 
+                print(f'{list(cardapioLista[1].keys())[0]} R$ {list(cardapioLista[1].values())[0]}')
+                quant = int(input("Digite a Quantidade do produto: "))
+                pedidoAtual.append(quant*list(cardapioLista[1].values())[0])
+                
+                TotalSemTaxa = sum(pedidoAtual)
+                print(f"Valor Total: R${TotalSemTaxa}")
+                
+                print(sep)
+
+            case 202:
+                print(f'{list(cardapioLista[2].keys())[0]} R$ {list(cardapioLista[2].values())[0]}')
+                quant = int(input("Digite a Quantidade do produto: "))
+                pedidoAtual.append(quant*list(cardapioLista[2].values())[0])
+                
+                TotalSemTaxa = sum(pedidoAtual)
+                print(f"Valor Total: R${TotalSemTaxa}")
+                
+                print(sep)
+
+            case 203:
+                print(f'{list(cardapioLista[3].keys())[0]} R$ {list(cardapioLista[3].values())[0]}')
+                quant = int(input("Digite a Quantidade do produto: "))
+                pedidoAtual.append(quant*list(cardapioLista[3].values())[0])
+
+                TotalSemTaxa = sum(pedidoAtual)
+                print(f"Valor Total: R${TotalSemTaxa}")
+                
+                print(sep)               
+    
+            case 204:
+                print(f'{list(cardapioLista[4].keys())[0]} R$ {list(cardapioLista[4].values())[0]}')
+                quant = int(input("Digite a Quantidade do produto: "))
+                pedidoAtual.append(quant*list(cardapioLista[4].values())[0])
+
+                TotalSemTaxa = sum(pedidoAtual)
+                print(f"Valor Total: R${TotalSemTaxa}")
+                
+                print(sep)
+
+            case 205:
+                print(f'{list(cardapioLista[5].keys())[0]} R$ {list(cardapioLista[5].values())[0]}')
+                quant = int(input("Digite a Quantidade do produto: "))
+                pedidoAtual.append(quant*list(cardapioLista[5].values())[0])
+
+                TotalSemTaxa = sum(pedidoAtual)
+                print(f"Valor Total: R${TotalSemTaxa}")
+                
+                print(sep)
+                
+            
+
+        
+
+opcao = True
+
+while(opcao):
+
+    print(sep)
+    taxaGarcom = int(input(f"Aceita pagar a gorjeta do garçom referente a 10% sobre o valor dos pratos? \n1 - Não \n2 - Sim \n"))
+    print(sep)
+
+    if(taxaGarcom == 1):
+        TotalSemTaxa = sum(pedidoAtual)
+        print(f"O valor Total do pedido Sem a taxa do garçom ficou em: R${TotalSemTaxa:.2f}")
+        print(sep)
+        opcao = False
+
+    elif(taxaGarcom == 2):
+        TotalComTaxa = sum(pedidoAtual) * 1.10
+        print(f"O valor Total do pedido com a taxa do garçom ficou em: R${TotalComTaxa:.2f}")
+        print(sep)
+        opcao = False
+    else:
+        print("Opção inválida, favor digitar uma opção válida")
+        print(sep)

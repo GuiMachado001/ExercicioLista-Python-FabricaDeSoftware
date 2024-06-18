283433a63d07e29177308f4dd83fbf8666d6a6da
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2024-06-18 11:34:03 -0400

    .

--- Lista 3/21.py
+++ Lista 3/21.py
@@ -13,12 +13,23 @@ contador = maior
 par = 0
 impar = 0
 
+soma = 0
+mult = 1
+
 while(contador >= menor):
     if(contador %2 == 0):
+        soma += contador
         par += 1
     else:
+        mult *= contador
         impar +=1
     contador -=1
 
+
+
 print("Quantidade de números pares:", par)
+print("A soma dos números pares:", soma)
+
+
 print("Quantidade de números ímpares:", impar)
+print("multiplicação do números ímpares:", mult)

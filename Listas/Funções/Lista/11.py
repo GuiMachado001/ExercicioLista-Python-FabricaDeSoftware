98788e5137bfbe51eb2323b24a5d26caa131145c
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2024-08-06 07:34:56 -0400

    Update 11.py

---
+++ Listas/Funções/Lista/11.py
@@ -0,0 +1,12 @@
+# Elabore uma função que receba dois valores numéricos e um símbolo. Este símbolo
+# representara a operação que se deseja efetuar com os números. Se o símbolo for + deverá ser
+# realizada uma adição, se for − uma subtração, se for / uma divisão e se for ∗ será efetuada uma
+# multiplicação.
+
+
+operacao = input("Digite a operação (Ex: 2+2): ")
+
+if(operacao == '+'):
+    print("mais")
+else:
+    print("outra")
